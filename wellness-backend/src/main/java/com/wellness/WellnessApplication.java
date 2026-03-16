8a14d2f1a6905285dd2be9592769efc6910778ef
Shivani Patil <shivanikpatil126@gmail.com>
2026-03-16 16:41:41 +0530

    Add BCryptPasswordEncoder and update createAdmin method

--- wellness-backend/src/main/java/com/wellness/WellnessApplication.java
+++ wellness-backend/src/main/java/com/wellness/WellnessApplication.java
@@ -4,27 +4,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.wellness.entity.User;
 import com.wellness.repository.UserRepository;
+
 @SpringBootApplication
 public class WellnessApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WellnessApplication.class, args);
     }
-    
+
     @Bean
-CommandLineRunner createAdmin(UserRepository userRepo, PasswordEncoder encoder) {
-    return args -> {
-        if (!userRepo.existsByEmail("shivanikpatil126@gmail.com")) {
-            userRepo.save(new User(
-                "shivanikpatil126@gmail.com",
-                encoder.encode("admin123"),
-                "ADMIN"
-            ));
-        }
-    };
-}
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CommandLineRunner createAdmin(UserRepository userRepo, PasswordEncoder encoder) {
+        return args -> {
+            if (!userRepo.existsByEmail("shivanikpatil126@gmail.com")) {
+                userRepo.save(new User(
+                    "shivanikpatil126@gmail.com",
+                    encoder.encode("admin123"),
+                    "ADMIN"
+                ));
+            }
+        };
+    }
 }
