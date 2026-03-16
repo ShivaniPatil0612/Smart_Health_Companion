77afc6ef68479c78520c071ac417add0a4d00c3a
Shivani Patil <shivanikpatil126@gmail.com>
2026-03-16 16:58:11 +0530

    Remove password encoder and admin setup code
    Removed unused password encoder and admin creation logic.

--- wellness-backend/src/main/java/com/wellness/WellnessApplication.java
+++ wellness-backend/src/main/java/com/wellness/WellnessApplication.java
@@ -2,13 +2,6 @@ package com.wellness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.wellness.entity.User;
-import com.wellness.repository.UserRepository;
 
 @SpringBootApplication
 public class WellnessApplication {
@@ -17,21 +10,4 @@ public class WellnessApplication {
         SpringApplication.run(WellnessApplication.class, args);
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    CommandLineRunner createAdmin(UserRepository userRepo, PasswordEncoder encoder) {
-        return args -> {
-            if (!userRepo.existsByEmail("shivanikpatil126@gmail.com")) {
-                userRepo.save(new User(
-                    "shivanikpatil126@gmail.com",
-                    encoder.encode("admin123"),
-                    "ADMIN"
-                ));
-            }
-        };
-    }
 }
