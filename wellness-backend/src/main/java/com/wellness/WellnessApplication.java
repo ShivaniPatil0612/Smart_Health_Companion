635ff2d73599b90a5ab630bfc0bbd12761ecae88
Shivani Patil <shivanikpatil126@gmail.com>
2026-03-16 16:05:10 +0530

    Implement admin user creation on startup
    Add CommandLineRunner to create an admin user if not exists.

--- wellness-backend/src/main/java/com/wellness/WellnessApplication.java
+++ wellness-backend/src/main/java/com/wellness/WellnessApplication.java
@@ -9,4 +9,17 @@ public class WellnessApplication {
     public static void main(String[] args) {
         SpringApplication.run(WellnessApplication.class, args);
     }
+    
+    @Bean
+CommandLineRunner createAdmin(UserRepository userRepo, PasswordEncoder encoder) {
+    return args -> {
+        if (!userRepo.existsByEmail("shivanikpatil126@gmail.com")) {
+            userRepo.save(new User(
+                "shivanikpatil126@gmail.com",
+                encoder.encode("admin123"),
+                "ADMIN"
+            ));
+        }
+    };
+}
 }
